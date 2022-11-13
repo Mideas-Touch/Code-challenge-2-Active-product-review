e68d382c5114bb0154ab769290c15861077d3882
fliblip <wickynjy@gmail.com>
2022-11-13 22:14:06 +0300

    Add #favorite_product

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
     has_many :reviews
     has_many :products, through: :reviews
+
+    # favorite_product_id = Review.find_by(star_rating: reviews.maximum(:star_rating)).product_id
+    #     Product.find(favorite_product_id)
+
+    def favorite_product
+        self.reviews.order(:star_rating).last.product
+    end
+
 end
