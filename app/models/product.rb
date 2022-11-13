32840950673be83f1fa0cc79dcf223da1697cc00
fliblip <wickynjy@gmail.com>
2022-11-13 19:33:19 +0300

    Add print_all_reviews method

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,14 @@
 class Product < ActiveRecord::Base
     has_many :users, through: :reviews
     has_many :reviews
+
+    def leave_review(user:, star_rating:, comment:)
+        Review.create(user_id: user.id, product_id: self.id, star_rating: star_rating, comment: comment)
+    end
+
+    def print_all_reviews
+        self.reviews.map do |review|
+            puts "Review for #{review.product.name} by #{review.user.name}: #{review.star_rating}. #{review.comment}"
+        end
+    end
 end
